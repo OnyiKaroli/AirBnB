6d47e818724cf2b53862a61d296552220d24069c
Karoli Onyango <karolionyango@gmail.com>
2023-08-31 11:28:27 +0300

    Header modification

--- header_v2.php
+++ header_v2.php
@@ -1,28 +1,25 @@
-<header class="header-nav nav-homepage-style stricky main-menu">
+<header class="header-nav nav-innerpage-style main-menu">
     <!-- Ace Responsive Menu -->
     <nav class="posr"> 
       <div class="container posr menu_bdrt1">
         <div class="row align-items-center justify-content-between">
           <div class="col-auto">
-            <div class="d-flex align-items-center justify-content-between">
-              <div class="logos mr40">
-                <a class="header-logo logo1" href="index.php"><img src="images/header-logo.svg" alt="Header Logo"></a>
-                <a class="header-logo logo2" href="index.php"><img src="images/header-logo2.svg" alt="Header Logo"></a>
-              </div>
-              <!-- Responsive Menu Structure-->
-              <ul id="respMenu" class="ace-responsive-menu" data-menu-style="horizontal">
+            <a class="header-logo" href="index.html"><img src="images/header-logo2.svg" alt="Header Logo"></a>
+          </div>
+          <div class="col-auto">
+            <!-- Responsive Menu Structure-->
+            <ul id="respMenu" class="ace-responsive-menu" data-menu-style="horizontal">
                 <li class="visible_list"><a class="list-item" href="index.php"><span class="title">Home</span></a></li>
                 <li class="megamenu_style"><a  class="list-item" href="listing.php"><span class="title">Listing</span></a></li>
                 <li class="visible_list"><a class="list-item" href="agency.php"><span class="title">Agencies</span></a></li>
                 <li class="visible_list"><a class="list-item" href="blog.php"><span class="title">Blog</span></a></li>
-              </ul>
-            </div>
+            </ul>
           </div>
           <div class="col-auto">
             <div class="d-flex align-items-center">
               <a class="login-info d-flex align-items-center" data-bs-toggle="modal" href="#exampleModalToggle" role="button"><i class="far fa-user-circle fz16 me-2"></i> <span class="d-none d-xl-block">Login / Register</span></a>
-              <a class="ud-btn add-property menu-btn bdrs60 mx-2 mx-xl-4" href="page-dashboard-add-property.html">Add Property<i class="fal fa-arrow-right-long"></i></a>
-              <a class="sidemenu-btn filter-btn-right" href="#"><img class="img-1" src="images/icon/nav-icon-white.svg" alt=""><img class="img-2" src="images/icon/nav-icon-dark.svg" alt=""></a>
+              <a class="ud-btn btn-transparent add-property bdrs60 mx-2 mx-xl-4" href="page-dashboard-add-property.html">Add Property<i class="fal fa-arrow-right-long"></i></a>
+              <a class="sidemenu-btn filter-btn-right" href="#"><img src="images/dark-nav-icon.svg" alt=""></a>
             </div>
           </div>
         </div>
